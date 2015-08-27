a45813d20504b1c1ec53f6f05b89ebd5b85c6035
Ryan Lane <ryan@ryanalane.com>
2015-08-27 09:21:53 +0200

    Commented out feature code to force tests to fail on CI server

--- features/landing-page/server/landing-page.js
+++ features/landing-page/server/landing-page.js
@@ -1,6 +1,6 @@
 Meteor.startup(function () {
-  Pages.upsert({path: "/"}, {
-    path: "/",
-    heading: "Neverpress by Xolv.io"
-  });
+//  Pages.upsert({path: "/"}, {
+//    path: "/",
+//    heading: "Neverpress by Xolv.io"
+//  });
 });
